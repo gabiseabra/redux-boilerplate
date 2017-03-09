8bea219c25c741665556491b93325487ee729477
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-03-09 19:31:13 -0300

    Added brand to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router"
 import Nav from "./Nav"
 import styles from "./Header.css"
 
-const Header = ({ className }) => (
+const Header = ({ className }, { data }) => (
 	<header className={[ styles.Header, className ].join(" ")}>
+		<Link to="/" className={styles.brand}>{data.title}</Link>
 		<Nav>
 			<Link to="/">Home</Link>
 			<Link to="/hello">Hello</Link>
@@ -14,6 +15,10 @@ const Header = ({ className }) => (
 
 Header.propTypes = {
 	className: PropTypes.string
+}
+
+Header.contextTypes = {
+	data: PropTypes.object.isRequired
 }
 
 export default Header
