55f2a68b6172475cad9d5f9fd861efd3b185c9cd
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-03-19 03:35:57 -0300

    Added hydrate select method

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,7 @@
 export const HYDRATE = "HYDRATE"
 
-export const hydrate = (payload) => ({ type: HYDRATE, payload })
+export const hydrate = (payload) => ({
+	type: HYDRATE,
+	payload,
+	select: (selector) => (selector(payload))
+})
