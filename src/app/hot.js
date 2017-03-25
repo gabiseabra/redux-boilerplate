c3c9e27723fdb75fb74cd0fff944d7fc09eb1e8d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-03-25 00:37:38 -0300

    Fixed css in hot module replacement

---
+++ src/app/hot.js
@@ -0,0 +1,20 @@
+export default function sync(reporter, timeout = 2000) {
+	const next = reporter.success
+	// eslint-disable-next-line no-param-reassign
+	reporter.success = function () {
+		document.querySelectorAll("link[href][rel=stylesheet]").forEach((link) => {
+			if(!/$https?:\/\//.test(link.href)) {
+				const href = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
+				const newLink = link.cloneNode()
+				newLink.href = href
+				link.parentNode.appendChild(newLink)
+				setTimeout(() => {
+					if(link.parentNode) {
+						link.parentNode.removeChild(link)
+					}
+				}, timeout)
+			}
+		})
+		next()
+	}
+}
--- src/client.jsx
+++ src/client.jsx
@@ -4,6 +4,7 @@ import { Router, browserHistory } from "react-router"
 import * as OfflinePlugin from "offline-plugin/runtime"
 import Provider from "./app/Provider"
 import routes from "./app/routes"
+import sync from "./app/hot"
 
 OfflinePlugin.install()
 
@@ -20,24 +21,6 @@ ReactDOM.render(
 	document.getElementById("app")
 )
 
-if(module.hot) {
-	const reporter = window.__webpack_hot_middleware_reporter__
-	const next = reporter.success
-	const DEAD_TIMEOUT = 2000
-
-	reporter.success = function () {
-		document.querySelectorAll("link[href][rel=stylesheet]").forEach((link) => {
-			const href = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
-			const newLink = link.cloneNode()
-			newLink.href = href
-			link.parentNode.appendChild(newLink)
-			setTimeout(() => {
-				if(link.parentNode) {
-					link.parentNode.removeChild(link)
-				}
-			}, DEAD_TIMEOUT)
-			next()
-		})
-	}
-	module.hot.accept()
+if(process.env.NODE_ENV === "production" && module.hot) {
+	sync(window.__webpack_hot_middleware_reporter__)
 }
