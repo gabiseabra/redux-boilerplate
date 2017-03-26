52caa1b6a0c0892d06b0e18a155c85910b7fbf91
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-03-26 01:04:00 -0300

    Added environment plugin to development

--- webpack/config/development.js
+++ webpack/config/development.js
@@ -5,6 +5,9 @@ export const CSS_MODULE_NAME = "[name]_[local]--[hash:base64:5]"
 export default {
 	devtool: "inline-source-map",
 	plugins: [
-		new webpack.NamedModulesPlugin()
+		new webpack.NamedModulesPlugin(),
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: "production"
+		})
 	]
 }
