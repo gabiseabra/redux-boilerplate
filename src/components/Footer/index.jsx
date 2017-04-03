4c447fcf594e2f9d86385fa3ec3e00c43e153a27
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-04-02 23:26:14 -0300

    Pure component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from "react"
+import React, { PureComponent, PropTypes } from "react"
 import styles from "./Footer.css"
 
-export default class Footer extends Component {
+export default class Footer extends PureComponent {
 	/* Add social networks here */
 	static Networks = [
 		{
