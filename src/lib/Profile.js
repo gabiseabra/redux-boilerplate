17abe32131cedf88a88a5a67df5de5d11665d543
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-04-10 07:29:05 -0300

    Bug fix

--- src/lib/Profile.js
+++ src/lib/Profile.js
@@ -16,10 +16,11 @@ export default class Profile {
 	}
 
 	social(domain) {
-		if(this.info.sameAs) {
+		const { sameAs } = this.info
+		if(sameAs) {
 			const regExp = new RegExp(`^https?:\\/\\/([^\\.]+\\.)*${domain.replace(/[^\w\s]/g, "\\$&")}`)
-			for(let i = 0; i < this.info.sameAs.length; ++i) {
-				const url = this.sameAs[i]
+			for(let i = 0; i < sameAs.length; ++i) {
+				const url = sameAs[i]
 				if(url.match(regExp)) {
 					return url
 				}
